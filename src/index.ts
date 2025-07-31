aeb6a0105e871f6db45009d7dd632b070862e23f
Aung Thu <dev104.ovmyanmar@gmail.com>
2025-07-31 13:09:04 +0630

    typescript array learning

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-//num declare
-let num : number = 10;
+//string
+let person : string = "Aung Thu";
+person = "royal";
+// person =  25; 
+
+//number
+let age : number = 25;
+age = 26;
+// age = "22";
+
+//boolean
+let passed : boolean = false;
+passed = true;
+// passed = "false";
